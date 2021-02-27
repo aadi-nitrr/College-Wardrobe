51f0d26e63a052c6943da91815c045cedb0678a5
Shashank Shekhar <ssshekhu53@gmail.com>
2021-02-27 16:20:22 +0530

    Organised directory structure, rectified unclosed tags, used jQuery instead of pure javascript, added cdn of bootstrap

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    padding: 10,
+    nav:false,
+    autoplay: true,
+    stagePadding:50,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:7
+        }
+    }
+});
+
+$(document).on('click', '#signUp', function(){
+    $('#login .container').addClass('right-panel-active');
+});
+
+$(document).on('click', '#signIn', function(){
+    $('#login .container').removeClass('right-panel-active');
+});
